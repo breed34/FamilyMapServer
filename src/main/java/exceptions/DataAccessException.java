b8590e0d49ca9eb0e57008bee08ee694da7962e3
Briggs Reed <breed34@byu.edu>
2023-01-28 13:06:37 -0700

    Add JavaDoc and constructors for DataAccessException

--- src/main/java/exceptions/DataAccessException.java
+++ src/main/java/exceptions/DataAccessException.java
@@ -1,4 +1,22 @@
 package exceptions;
 
+/**
+ * An exception to be thrown when an error happens during a database transaction.
+ */
 public class DataAccessException extends Exception {
+    /**
+     * Creates a DataAccessException with no message.
+     */
+    public DataAccessException() {
+        super();
+    }
+
+    /**
+     * Creates a DataAccessException with a message.
+     *
+     * @param message the message to send with the exception.
+     */
+    public DataAccessException(String message) {
+        super(message);
+    }
 }
